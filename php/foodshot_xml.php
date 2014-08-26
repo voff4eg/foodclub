7b6250baed409a2965e4f5d88962c217dc1cf098
root <root@foodclub.ru>
2014-08-26 15:51:04 +0400

    foodshot xml script

--- php/foodshot_xml.php
+++ php/foodshot_xml.php
@@ -10,15 +10,20 @@ $CFooshot = CFoodshot::getInstance();
 $arFoodshotList = $CFooshot->getList(array(),0);
 if(!empty($arFoodshotList["elems"])){
 	foreach($arFoodshotList["elems"] as $i => $arFoodshot){
+		$update_time = MakeTimeStamp(ConvertDateTime($arFoodshot["date_update"], "DD.MM.YYYY HH:MI:SS"), "DD.MM.YYYY HH:MI:SS");
+		//1409051419 26-08-14 15-10
+		if($update_time > 1409051419){
+			$time = $update_time;
+		}else{
+			$time = 1409051419;
+		}
 		$strFoodshotXML .= "
-<url>
-	<loc>http://www.foodclub.ru/foodshot/".$arFoodshot["id"]."/#!foodshot</loc>
-	<lastmod>".TimeEncode(MakeTimeStamp(ConvertDateTime($arFoodshot["date_update"], "DD.MM.YYYY HH:MI:SS"), "DD.MM.YYYY HH:MI:SS"))."</lastmod>
-</url>";
+	<url>
+		<loc>http://www.foodclub.ru/foodshot/".$arFoodshot["id"]."/#!foodshot</loc>
+		<lastmod>".TimeEncode($time)."</lastmod>
+	</url>";
 	}
 }
-
-echo $strFoodshotXML;
 
 //TimeEncode(MakeTimeStamp(ConvertDateTime($ar["FULL_DATE_CHANGE"], "DD.MM.YYYY HH:MI:SS"), "DD.MM.YYYY HH:MI:SS"))
 function TimeEncode($iTime)
@@ -29,4 +34,15 @@ function TimeEncode($iTime)
 	$strTZ = ($iTZ<0? "-": "+").sprintf("%02d:%02d", $iTZHour, $iTZMinutes);
 	return date("Y-m-d",$iTime)."T".date("H:i:s",$iTime).$strTZ;
 }
+
+$str = '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+$str .= $strFoodshotXML;
+$str .= '
+</urlset>';
+
+chdir ('/srv/www/foodclub/');
+$file = fopen('yandex_foodshot.xml', 'w');
+fwrite($file,$str);
+fclose($file);
 ?>
